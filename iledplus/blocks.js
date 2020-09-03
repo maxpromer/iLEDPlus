4f8de4c8683ace852b875736c15368abaf2c3cb6
Max <max30012540@hotmail.com>
2020-09-03 19:39:15 +0700

    เพิ่มภาษาไทย

--- iledplus/blocks.js
+++ iledplus/blocks.js
@@ -1,18 +1,45 @@
 Blockly.Blocks['ledplus'] = {
   init: function() {
-    this.appendValueInput("number")
-        .setCheck("Number")
-        .appendField("LED 16x8 show number");
-    this.appendDummyInput()
-        .appendField("base")
-        .appendField(new Blockly.FieldDropdown([["DEC","10"], ["BIN","2"], ["OCT","8"], ["HEX","16"]]), "base");
-    this.setInputsInline(true);
-    this.setPreviousStatement(true, null);
-    this.setNextStatement(true, null);
-    this.setColour(165);
-    this.setTooltip("Show number in LED 16x8");
-    this.setHelpUrl("");
-  },
+    this.jsonInit({
+      "type": "ledplus",
+      "message0": Blockly.Msg.iLEDPlus_MESSAGE,
+      "args0": [
+        {
+          "type": "input_value",
+          "name": "number",
+          "check": "Number"
+        },
+        {
+          "type": "field_dropdown",
+          "name": "base",
+          "options": [
+            [
+              "10",
+              "10"
+            ],
+            [
+              "2",
+              "2"
+            ],
+            [
+              "8",
+              "8"
+            ],
+            [
+              "16",
+              "16"
+            ]
+          ]
+        }
+      ],
+      "inputsInline": true,
+      "previousStatement": null,
+      "nextStatement": null,
+      "colour": 165,
+      "tooltip": Blockly.Msg.iLEDPlus_TOOLTIP,
+      "helpUrl": ""
+    });
+	},
 	xmlToolbox: function() {
 		return `
 		<block type="ledplus">
